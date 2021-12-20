79c5b93b77608340f6bd0f2adbbe692d53264d79
jake.k <jake.k@hpcnt.com>
2021-12-20 15:31:03 +0900

    Initial Commit

---
+++ ExAlignedCollectionViewFlowLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExAlignedCollectionViewFlowLayout
+//
+//  Created by Jake.K on 2021/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
